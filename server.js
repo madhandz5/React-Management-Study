da2539e0aca37e5b5cf25c210c705101fd5b1c64
madhandz5 <madhandz5@naver.com>
2020-06-12 05:03:34 +0900

    Add Get Customers REST API Module

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-    res.send({ message: "Hello Express!" });
+app.get('/api/customers',(req,res)=>{
+    res.send([
+        {
+            "id": 1,
+            "image": "https://placeimg.com/64/64/1",
+            "name": "홍길동",
+            "birthday": "961222",
+            "gender": "남자",
+            "job": "대학생"
+          },
+          {
+            "id": 2,
+            "image": "https://placeimg.com/64/64/2",
+            "name": "오창영",
+            "birthday": "860522",
+            "gender": "남자",
+            "job": "개발자"
+          },
+          {
+            "id": 3,
+            "image": "https://placeimg.com/64/64/3",
+            "name": "오주혁",
+            "birthday": "191014",
+            "gender": "남자",
+            "job": "아기"
+          }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
